f272240385089abbaf1f7266ffb9875cf0ec02e7
零零豆 <29398217@qq.com>
2023-06-28 01:17:30 +0800

    Update sidebar.ts

--- docs/.vuepress/sidebar.ts
+++ docs/.vuepress/sidebar.ts
@@ -2,12 +2,11 @@ import { sidebar } from "vuepress-theme-hope";
 
 export default sidebar({
   
-  "guide": [
+  "/": [
     {
       text: "U盘手册",
       icon: "newspaper",
       prefix: "guide/",
-      link: "/guide/产品前言.html",
       children: ['/guide/产品前言.html', '/guide/操作手册.html','/guide/激活工具使用.md'],
     },
   ],
