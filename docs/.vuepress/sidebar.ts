75f5c77b9973adcb94ac359ae882534b5ef0c446
零零豆 <29398217@qq.com>
2023-06-27 16:59:40 +0800

    Update sidebar.ts

--- docs/.vuepress/sidebar.ts
+++ docs/.vuepress/sidebar.ts
@@ -2,11 +2,7 @@ import { sidebar } from "vuepress-theme-hope";
 
 export default sidebar({
   "/": [
-    {
-      text: "首页",
-      icon: "home",
-      link: "/",
-    },
+    "/",
     {
       text: "U盘手册",
       icon: "newspaper",
