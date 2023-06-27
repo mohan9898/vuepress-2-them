db7559b3477ee62b3e91a20a524b90090cf6e3ba
零零豆 <29398217@qq.com>
2023-06-28 01:18:13 +0800

    Update sidebar.ts

--- docs/.vuepress/sidebar.ts
+++ docs/.vuepress/sidebar.ts
@@ -6,7 +6,7 @@ export default sidebar({
     {
       text: "U盘手册",
       icon: "newspaper",
-      prefix: "true,
+      prefix: true,
       children: ['/guide/产品前言.html', '/guide/操作手册.html','/guide/激活工具使用.md'],
     },
   ],
