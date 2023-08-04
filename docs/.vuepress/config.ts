25d76e03eb341ef5f8329b8d3ecec05a4b405542
零零豆 <29398217@qq.com>
2023-08-05 00:23:55 +0800

    更新 config.ts

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -1,8 +1,12 @@
 import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
+import { defineClientConfig } from '@vuepress/client'
+import GlobalPopup from './components/GlobalPopup.vue'
 
 export default defineUserConfig({
   
+  rootComponents: [GlobalPopup],
+
   base: "/",
   title: '惊涵系统U盘',
   head: [['link', { rel: 'icon', href: 'https://sihengl.gitee.io/jinghan/logo/Upan.png' }]],
