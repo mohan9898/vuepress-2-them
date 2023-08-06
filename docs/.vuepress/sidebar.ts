16a32efde966d42f4a1a2f7cac7b6b214f3fe588
零零豆 <29398217@qq.com>
2023-08-06 21:13:15 +0800

    Update sidebar.ts

--- docs/.vuepress/sidebar.ts
+++ docs/.vuepress/sidebar.ts
@@ -5,12 +5,14 @@ export default sidebar({
     "/",
     {
       text: "U盘手册",
+      collapsible: "true",
       icon: "newspaper",
       prefix: "/guide/upan/",
       children: ['产品前言', '操作手册','激活工具使用','安装注意',],
     },
     {
       text: "文件使用",
+      collapsible: "true",
       icon: "newspaper",
       prefix: "/guide/file/",
       children: ['系统镜像下载',],
